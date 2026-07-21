066488cf25f79af796f86c362718858f0ac6f456
Turhan Can Kargin <turhancan.kargin@gmail.com>
2026-07-21 13:55:42 +0200

    refactor: remove project documentation, redundant test runners, and unused assets

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist', 'node_modules', 'test_runner.js', 'test_runner.ts'] },
+  { ignores: ['dist', 'node_modules'] },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
